2bb93576c5ae2dcf0708c10739545e0394c88d26
Colin797 <colin797@tamu.edu>
2019-04-21 10:01:23 -0500

    added list clicking functionality

--- event/src/components/Fest.js
+++ event/src/components/Fest.js
@@ -1,21 +1,39 @@
 import React from "react";
+import { Redirect } from "react-router-dom"
 
 class Fest extends React.Component{
+	state = {
+		toInfo: false
+	}
+
+	getInfo(num){
+		this.setState({
+			toInfo: true
+		})
+	}
+
+	renderRedirect(){
+		if(this.state.toInfo){	
+			return <Redirect to='/info'/>
+		}
+	}
+
 	render(){
 		return(
 			<div>
+				{ this.renderRedirect() }
 				<h1> Select an event from the list below to receive more information! </h1>
 				<ul>
-					<li>{this.props.titles[0]}</li>
-					<li>{this.props.titles[1]}</li>
-					<li>{this.props.titles[2]}</li>
-					<li>{this.props.titles[3]}</li>
-					<li>{this.props.titles[4]}</li>
-					<li>{this.props.titles[5]}</li>
-					<li>{this.props.titles[6]}</li>
-					<li>{this.props.titles[7]}</li>
-					<li>{this.props.titles[8]}</li>
-					<li>{this.props.titles[9]}</li>
+					<li onClick={() => this.getInfo(0)}>{this.props.titles[0]}</li>
+					<li onClick={() => this.getInfo(1)}>{this.props.titles[1]}</li>
+					<li onClick={() => this.getInfo(2)}>{this.props.titles[2]}</li>
+					<li onClick={() => this.getInfo(3)}>{this.props.titles[3]}</li>
+					<li onClick={() => this.getInfo(4)}>{this.props.titles[4]}</li>
+					<li onClick={() => this.getInfo(5)}>{this.props.titles[5]}</li>
+					<li onClick={() => this.getInfo(6)}>{this.props.titles[6]}</li>
+					<li onClick={() => this.getInfo(7)}>{this.props.titles[7]}</li>
+					<li onClick={() => this.getInfo(8)}>{this.props.titles[8]}</li>
+					<li onClick={() => this.getInfo(9)}>{this.props.titles[9]}</li>
 				</ul>
 			</div>
 			)
